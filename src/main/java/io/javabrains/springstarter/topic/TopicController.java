4b95e8a51193d145f2bab72cc167a0b28927e793
Nikhil Dingane <nikhil@krishagni.com>
2020-09-08 10:08:34 +0530

    creating, updating and deleting resource REST APIs

--- src/main/java/io/javabrains/springstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springstarter/topic/TopicController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +16,7 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 	
-	@RequestMapping("/topics")
+	@RequestMapping(method = RequestMethod.GET, value = "/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getTopics();
 	}
@@ -22,5 +24,20 @@ public class TopicController {
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {		
 		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+		topic.setId(id);
+		topicService.updateTopic(topic,id);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public Topic deleteTopic(@PathVariable String id) {		
+		return topicService.deleteTopic(id);
 	}
 }
--- src/main/java/io/javabrains/springstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.javabrains.springstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList(Arrays.asList(
 			new Topic("Spring","Spring Framework","Spring Framwork Description"),
 			new Topic("Java", "Core Java", "Java Description"),
 			new Topic("Javascript", "Javascript", "Javascript Description")
-			);
+			));
 	
 	public List<Topic> getTopics(){
 		return topics;
@@ -26,5 +27,32 @@ public class TopicService {
 			}
 		}
 		return null;
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public Topic updateTopic(Topic topic,String id) {
+		for(Topic t : topics) {
+			System.out.print(topic.getId());
+			if(t.getId().equals(id)) {				
+				t.setName(topic.getName());
+				t.setDescription(topic.getDescription());
+				return t;
+			}
+		}
+		return null;
+	}
+	
+	public Topic deleteTopic(String id) {
+		Topic topic = null;
+		for (int i = 0; i < topics.size(); i++) {
+			if(topics.get(i).getId().equals(id)) {
+				topic = topics.remove(i);
+				break;
+			}
+		}
+		return topic;
 	}
 }
